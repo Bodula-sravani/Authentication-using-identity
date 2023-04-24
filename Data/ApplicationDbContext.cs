2a2a506af6ae55dd60f052ee6c78f589ef5d39b9
Bodula-sravani <sravanibodula@gmail.com>
2023-04-24 10:42:38 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Auth.Models;
+
+namespace Auth.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+
+
+		}
+		public DbSet<Auth.Models.Student>? Student { get; set; }
+	}
+}
